117590e71ebfdfdb7d165abf13d12532a42e0440
Nicolas Chaulet <n.chaulet@gmail.com>
2017-04-29 12:55:35 -0400

    Remove jumbotron

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var langs = ['fr', 'en'];
 var defaultLang = 'fr';
 
 langs.forEach(function(lang) {
-    var resumeJson = require('./data/resume-' + lang + '.json');
+  var resumeJson = require('./data/resume-' + lang + '.json');
 
-    var fileName = (lang == defaultLang ? 'index' : lang) + '.html';
-    fs.writeFileSync(__dirname + '/dist/' + fileName, theme.render(resumeJson, lang));
+  var fileName = (lang == defaultLang ? 'index' : lang) + '.html';
+  fs.writeFileSync(__dirname + '/dist/' + fileName, theme.render(resumeJson, lang));
 });
 
--- theme/index.js
+++ theme/index.js
@@ -4,67 +4,67 @@ var I18n = require('i18n-js');
 var moment = require('moment');
 
 I18n.translations["en"] = {
-    title: {
-        work: 'Work',
-        education: 'Education',
-        skills: 'Skills',
-        language: 'Languages',
-        interest: 'Interest'
-    },
-    now: 'Now'
+  title: {
+    work: 'Work',
+    education: 'Education',
+    skills: 'Skills',
+    language: 'Languages',
+    interest: 'Interest'
+  },
+  now: 'Now'
 };
 
 I18n.translations["fr"] = {
-    title: {
-        work: 'Expériences',
-        education: 'Formations',
-        skills: 'Compétences',
-        language: 'Langues',
-        interest: 'Loisirs'
-    },
-    now: 'Aujourd\'hui'
+  title: {
+    work: 'Expériences',
+    education: 'Formations',
+    skills: 'Compétences',
+    language: 'Langues',
+    interest: 'Loisirs'
+  },
+  now: 'Aujourd\'hui'
 };
 
 Handlebars.registerHelper('I18n',
   function(str){
     return I18n.t(str);
   }
-);
+  );
 
 Handlebars.registerHelper('datePeriod',
   function(startDate, endDate){
     if (!endDate) {
-        endDate = I18n.t('now');
+      endDate = I18n.t('now');
 
-        return moment(startDate).format('MM/YYYY') + ' - ' + endDate;
+      return moment(startDate).format('MM/YYYY') + ' - ' + endDate;
     }
 
     var startDate = moment(startDate);
     var endDate = moment(endDate);
     if (endDate.diff(startDate, 'months') > 8) {
-        return startDate.format('YYYY') + ' - ' + endDate.format('YYYY');
+      return startDate.format('YYYY') + ' - ' + endDate.format('YYYY');
     } else {
-        return startDate.format('MM/YYYY') + ' - ' + endDate.format('MM/YYYY');
+      return startDate.format('MM/YYYY') + ' - ' + endDate.format('MM/YYYY');
     }
   }
-);
+  );
 
 function render(resume, locale) {
-    if (!locale) {
-        locale = 'fr';
-    }
+  if (!locale) {
+    locale = 'fr';
+  }
 
-    I18n.locale = locale;
+  I18n.locale = locale;
 
-    var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-    var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
 
-    return Handlebars.compile(tpl)({
-        css: css,
-        resume: resume
-    });
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume
+  });
 }
 
 module.exports = {
-    render: render
+  render: render
 };
